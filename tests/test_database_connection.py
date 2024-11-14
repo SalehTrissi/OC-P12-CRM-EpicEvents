267f4bd5ee76a35c7285555663b8e42f361eb427
Saleh Trissi <rayatrissi1@gmail.com>
2024-11-14 22:07:00 +0100

    fix the error for test database connection

--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -1,19 +1,19 @@
 from sqlalchemy import text
-from db.database import session
+from db.database import SessionLocal
 from sqlalchemy.exc import OperationalError
 
 
 def test_database_connection():
     """Test if the database connection is working."""
     try:
-        session.execute(text("SELECT 1"))
+        SessionLocal.execute(text("SELECT 1"))
         print("Database connection successful")
         connection_status = True
     except OperationalError:
         print("Database connection failed")
         connection_status = False
     finally:
-        session.close()
+        SessionLocal.close()
         print("Database session closed")
 
     assert connection_status, "Database connection failed"
