61cb08fb9729bcd74cd6d1f938bbac82f1cf9cb2
Saleh Trissi <rayatrissi1@gmail.com>
2025-01-26 10:05:49 +0100

    Added Sentry integration: Configured Sentry initialization, added environment variables support, and created unit tests for Sentry functionality

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from dotenv import load_dotenv
+import sentry_sdk
 import os
 
 
@@ -14,3 +16,18 @@ if not JWT_SECRET:
 JWT_ALGORITHM = 'HS256'
 # Token validity period (in seconds)
 JWT_EXP_DELTA_SECONDS = 3600
+
+
+# Configuration de Sentry
+SENTRY_DSN = os.getenv('SENTRY_DSN')
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        integrations=[SqlalchemyIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True
+    )
+else:
+    print("SENTRY_DSN n'est pas défini. Sentry ne sera pas initialisé.")
---
+++ tests/test_sentry.py
@@ -0,0 +1,64 @@
+from sentry_sdk import capture_message, capture_exception, set_user, set_context
+from dotenv import load_dotenv
+import sentry_sdk
+import unittest
+import os
+
+
+load_dotenv()
+
+SENTRY_DSN = os.getenv('SENTRY_DSN')
+
+if not SENTRY_DSN:
+    print("SENTRY_DSN is not defined. Sentry will not be initialized.")
+else:
+    print("Sentry initialized successfully.")
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    traces_sample_rate=1.0
+)
+
+
+class TestSentryIntegration(unittest.TestCase):
+
+    def test_caputure_message(self):
+        try:
+            capture_message("Test message: Sentry is working correctly")
+            self.assertTrue(True)
+        except Exception as e:
+            self.fail(f"Failed to capture message: {e}")
+
+    def test_capture_exception(self):
+        try:
+            try:
+                1 / 0
+            except Exception as e:
+                capture_exception(e)
+                self.assertTrue(True)
+        except Exception as e:
+            self.fail(f"Failed to capture exception: {e}")
+
+    def test_user_context(self):
+        try:
+            set_user({"id": "12345", "email": "test_user@gmail.com",
+                     "username": "test_user"})
+            self.assertTrue(True)
+        except Exception as e:
+            self.fail(f"Failed to set user context: {e}")
+
+    def test_custom_context(self):
+        try:
+            set_context("custom_data", {
+                "key1": "value1",
+                "key2": "value2",
+                "important_flag": True
+            })
+            capture_message("Test message with custom context")
+            self.assertTrue(True)
+        except Exception as e:
+            self.fail(f"Failed to set custom context: {e}")
+
+
+if __name__ == '__main__':
+    unittest.main()
